1b0f340ee548a81007437ee7d06e23f335d25b67
Gafuroff-prog <gafuroffgaming@gmail.com>
2024-11-11 18:58:22 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+let userName = prompt("Ismingizni kiriting");
+let surName = prompt("Familiyangizni kiriting");
+let ageName = prompt("Yoshingizni kiriting");
+let dictrictName = prompt("Yashash manzilingizni kiriting");
+
+alert(`Malumotlaringizni kiritganingiz uchun rahmat ${userName}, ${surName}, ${ageName}, ${dictrictName}`)
+
